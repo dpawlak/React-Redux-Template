802fa1f0607daf7ceb6b17d2bbdc286719717edc
Daniel Pawlak <daniel@Daniels-iMac.hsd1.ca.comcast.net>
2021-07-08 17:40:45 -0700

    Set up payloads

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
-export const increment = () => {
+export const increment = (nr) => {
     return {
-        type: 'INCREMENT'
+        type: 'INCREMENT',
+        payload: nr
+    }
+}
+export const decrement = (nr) => {
+    return {
+        type: 'DECREMENT',
+        payload: nr
     }
 }
